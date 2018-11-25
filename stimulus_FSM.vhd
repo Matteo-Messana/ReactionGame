9b0f43dd6dc56079ddd86232166ae8154450c44e
alexgorkoff <32149507+alexgorkoff@users.noreply.github.com>
2018-11-24 20:53:44 -0700

    Create stimulus_FSM.vhd

---
+++ stimulus_FSM.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity stimulus is
+       PORT ( 
+              enable         :   in STD_LOGIC;
+              LED9to15       :   out STD_LOGIC;
+       );
+end stimulus;
+
+architecture Behavioral of stimulus is
+
+begin
+
+process(enable)
+begin
+    if(enable = '0') then
+        LED9to15  <= (others => '0');
+    elseif(enable = '1') then
+        LED9to15  <= (others => '1');
+    end if;
+end process;
+
+end Behavioral;
